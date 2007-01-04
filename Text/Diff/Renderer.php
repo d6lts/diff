a237ea5a9213b8b4e16829c45e53ee4f6de549bc
moshe weitzman <weitzman@cyrve.com>
2007-01-04 23:00:38 +0000

    update PEAR to latest. i fixed paths for include files and commented out _encode() method in inline.php because it was encoding all output. hope that doesn't disturb the diffs

--- Text/Diff/Renderer.php
+++ Text/Diff/Renderer.php
@@ -5,7 +5,7 @@
  * This class renders the diff in classic diff format. It is intended that
  * this class be customized via inheritance, to obtain fancier outputs.
  *
- * $Horde: framework/Text_Diff/Diff/Renderer.php,v 1.5 2004/10/13 09:30:20 jan Exp $
+ * $Horde: framework/Text_Diff/Diff/Renderer.php,v 1.12 2005/12/16 11:07:33 jan Exp $
  *
  * @package Text_Diff
  */
@@ -31,13 +31,30 @@ class Text_Diff_Renderer {
      * Constructor.
      */
     function Text_Diff_Renderer($params = array())
-    {
+		{
         foreach ($params as $param => $value) {
             $v = '_' . $param;
             if (isset($this->$v)) {
                 $this->$v = $value;
             }
         }
+    }
+
+    /**
+     * Get any renderer parameters.
+     *
+     * @return array  All parameters of this renderer object.
+     */
+    function getParams()
+    {
+        $params = array();
+        foreach (get_object_vars($this) as $k => $v) {
+            if ($k[0] == '_') {
+                $params[substr($k, 1)] = $v;
+            }
+        }
+
+        return $params;
     }
 
     /**
@@ -56,21 +73,23 @@ class Text_Diff_Renderer {
         $nlead = $this->_leading_context_lines;
         $ntrail = $this->_trailing_context_lines;
 
-        $this->_startDiff();
+        $output = $this->_startDiff();
 
-        foreach ($diff->getDiff() as $edit) {
+        $diffs = $diff->getDiff();
+        foreach ($diffs as $i => $edit) {
             if (is_a($edit, 'Text_Diff_Op_copy')) {
                 if (is_array($block)) {
-                    if (count($edit->orig) <= $nlead + $ntrail) {
+                    $keep = $i == count($diffs) - 1 ? $ntrail : $nlead + $ntrail;
+                    if (count($edit->orig) <= $keep) {
                         $block[] = $edit;
                     } else {
                         if ($ntrail) {
                             $context = array_slice($edit->orig, 0, $ntrail);
                             $block[] = &new Text_Diff_Op_copy($context);
                         }
-                        $this->_block($x0, $ntrail + $xi - $x0,
-                                      $y0, $ntrail + $yi - $y0,
-                                      $block);
+                        $output .= $this->_block($x0, $ntrail + $xi - $x0,
+                                                 $y0, $ntrail + $yi - $y0,
+                                                 $block);
                         $block = false;
                     }
                 }
@@ -97,53 +116,49 @@ class Text_Diff_Renderer {
         }
 
         if (is_array($block)) {
-            $this->_block($x0, $xi - $x0,
-                          $y0, $yi - $y0,
-                          $block);
+            $output .= $this->_block($x0, $xi - $x0,
+                                     $y0, $yi - $y0,
+                                     $block);
         }
 
-        return $this->_endDiff();
+        return $output . $this->_endDiff();
     }
 
     function _block($xbeg, $xlen, $ybeg, $ylen, &$edits)
     {
-        $this->_startBlock($this->_blockHeader($xbeg, $xlen, $ybeg, $ylen));
+        $output = $this->_startBlock($this->_blockHeader($xbeg, $xlen, $ybeg, $ylen));
+
         foreach ($edits as $edit) {
             switch (strtolower(get_class($edit))) {
             case 'text_diff_op_copy':
-                $this->_context($edit->orig);
+                $output .= $this->_context($edit->orig);
                 break;
 
             case 'text_diff_op_add':
-                $this->_added($edit->final);
+                $output .= $this->_added($edit->final);
                 break;
 
             case 'text_diff_op_delete':
-                $this->_deleted($edit->orig);
+                $output .= $this->_deleted($edit->orig);
                 break;
 
             case 'text_diff_op_change':
-                $this->_changed($edit->orig, $edit->final);
+                $output .= $this->_changed($edit->orig, $edit->final);
                 break;
-
-            default:
-                trigger_error("Unknown edit type", E_USER_ERROR);
             }
-
-            $this->_endBlock();
         }
+
+        return $output . $this->_endBlock();
     }
 
     function _startDiff()
     {
-        ob_start();
+        return '';
     }
 
     function _endDiff()
     {
-        $val = ob_get_contents();
-        ob_end_clean();
-        return $val;
+        return '';
     }
 
     function _blockHeader($xbeg, $xlen, $ybeg, $ylen)
@@ -160,40 +175,37 @@ class Text_Diff_Renderer {
 
     function _startBlock($header)
     {
-        echo $header . "\n";
+        return $header . "\n";
     }
 
     function _endBlock()
     {
+        return '';
     }
 
     function _lines($lines, $prefix = ' ')
     {
-        foreach ($lines as $line) {
-            echo "$prefix$line\n";
-        }
+        return $prefix . implode("\n$prefix", $lines) . "\n";
     }
 
     function _context($lines)
     {
-        $this->_lines($lines);
+        return $this->_lines($lines);
     }
 
     function _added($lines)
     {
-        $this->_lines($lines, '>');
+        return $this->_lines($lines, '>');
     }
 
     function _deleted($lines)
     {
-        $this->_lines($lines, '<');
+        return $this->_lines($lines, '<');
     }
 
     function _changed($orig, $final)
     {
-        $this->_deleted($orig);
-        echo "---\n";
-        $this->_added($final);
+        return $this->_deleted($orig) . "---\n" . $this->_added($final);
     }
 
 }
